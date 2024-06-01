0e62ab0b5b796bdadfd04a488f759a3f02633fdf
teo <theodoros.a.minas@gmail.com>
2024-06-01 18:00:27 +0200

    etsi giati etsi

--- src/main/java/com/amigoscode/demo/notify/Peos.java
+++ src/main/java/com/amigoscode/demo/notify/Peos.java
@@ -1,10 +1,25 @@
 package com.amigoscode.demo.notify;
 
+import java.util.Objects;
+
 public class Peos {
 
     private String ole;
 
     public Peos(String ole) {
         this.ole = ole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Peos peos = (Peos) o;
+        return Objects.equals(ole, peos.ole);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ole);
     }
 }
